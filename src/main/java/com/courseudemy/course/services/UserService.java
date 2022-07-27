4aef01137cb80ab7c24f40b63c6f6e9374b42abd
Higor Jacinto <higorjmsouza@gmail.com>
2022-07-26 22:08:54 -0300

    Exception handling - delete

--- src/main/java/com/courseudemy/course/services/UserService.java
+++ src/main/java/com/courseudemy/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.courseudemy.course.entities.User;
 import com.courseudemy.course.repositories.UserRepository;
+import com.courseudemy.course.services.exceptions.DatabaseException;
 import com.courseudemy.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,17 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	@SuppressWarnings("deprecation")
